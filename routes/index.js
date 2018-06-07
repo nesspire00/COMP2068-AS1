376de5be1b731400518bc53e4b703a4a0cf4cb40
Mykyta Naumenko <nesspire00@gmail.com>
2018-06-07 10:23:52 -0400

    Site structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'My Projects' });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
