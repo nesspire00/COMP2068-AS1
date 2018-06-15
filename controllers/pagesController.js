105c600d5b8e428643f29c7357cfee6603564820
Mykyta Naumenko <nesspire00@gmail.com>
2018-06-14 20:20:20 -0400

    Basic footer\header with MDL.IO framework

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -5,20 +5,20 @@ exports.getHome = (req, res, next) => {
 
 /* Display About Me page */
 exports.getAbout = (req, res, next) => {
-  res.render('about', { title: 'Home' });
+  res.render('about', { title: 'About Me' });
 };
 
 /* Display My Projects page */
 exports.getProjects = (req, res, next) => {
-  res.render('projects', { title: 'Home' });
+  res.render('projects', { title: 'My Projects' });
 };
 
 /* Display Services page */
 exports.getServices = (req, res, next) => {
-  res.render('services', { title: 'Home' });
+  res.render('services', { title: 'Services' });
 };
 
 /* Display Contact page */
 exports.getContact = (req, res, next) => {
-  res.render('contact', { title: 'Home' });
+  res.render('contact', { title: 'Contact' });
 };
